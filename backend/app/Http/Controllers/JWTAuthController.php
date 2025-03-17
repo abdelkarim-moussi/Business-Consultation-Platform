05800c7ad2c21bf731154ca3b83293285966863d
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-03-17 15:32:45 +0000

    user login using JWT

--- backend/app/Http/Controllers/JWTAuthController.php
+++ backend/app/Http/Controllers/JWTAuthController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Doctrine\Common\Lexer\Token;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JWTAuthController extends Controller
@@ -27,5 +31,30 @@ class JWTAuthController extends Controller
 
             return response()->json(compact('user','token'),201);
 
+    }
+
+    public function login(Request $request){
+
+        $credentials = $request->only('email','password');
+
+        try{
+
+            if(! $token = JWTAuth::attempt($credentials)){
+                return response()->json([
+                    'error'=>'invalid credentials'
+                ]);
+            }
+
+        $user = Auth::user();
+
+        $token = JWTAuth::claims(['accountType'=>$user->accountType])->fromUser($user);
+
+        return response()->json(compact('user','token'));
+
+        }catch(JWTException $e){
+
+            return response()->json(['error'=>'could not create token'],500);
+        }
+        
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('register',[JWTAuthController::class,'register']);
+Route::post('login',[JWTAuthController::class,'login']);
