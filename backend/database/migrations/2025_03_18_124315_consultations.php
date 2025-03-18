afe23878565929781623781ab16c0055f2696e7b
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-03-18 12:44:17 +0000

    create database migrations for consultants ;$, entrepreneurs and consultations

---
+++ backend/database/migrations/2025_03_18_124315_consultations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consultations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('consultant_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('entrepreneur_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->float('consultationDelay');
+            $table->dateTime('consultationDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consultations');
+    }
+};
