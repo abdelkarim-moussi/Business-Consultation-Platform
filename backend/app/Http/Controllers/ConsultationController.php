dc00864d8e26332d1b74d112096ec22b0aac5907
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-03-18 12:49:18 +0000

    display disponible consultations

--- backend/app/Http/Controllers/ConsultationController.php
+++ backend/app/Http/Controllers/ConsultationController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Consultation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ConsultationController extends Controller
 {
     
 
-    public function store(){
-        
+    public function index(){
+
+        $consulations = Consultation::all();
+
+        return response()->json(compact('consultations'));
+    }
+
+    public function show(Consultation $consulation){
+
+        $consulation = DB::table('consulations')->where('id',$consulation->id)->first();
+
+        return response()->json(compact('consultation'));
     }
 }
