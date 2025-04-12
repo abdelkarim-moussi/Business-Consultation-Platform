05ca28ada9b719a3244a15d46952bf7604f88cfc
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-04-12 15:11:34 +0000

    create an input component

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Input(
+  label,
+  id,
+  name,
+  value,
+  onChange,
+  type,
+  inputClasses,
+  divClasses
+) {
+  return (
+    <div className={`flex gap-2 ${divClasses}`}>
+      <label className="text-sm lowercase" htmlFor={name}>
+        {label}
+      </label>
+
+      <input
+        className={`w-full border border-black h-[35px] rounded-lg ${inputClasses}`}
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        type={type}
+      />
+    </div>
+  );
+}
