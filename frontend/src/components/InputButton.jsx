2a5d34e9a9121909a9a9816a471f572307e3fa4c
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-04-13 16:07:16 +0000

    add filter options and search

---
+++ frontend/src/components/InputButton.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const InputButton = ({type,placeholder,buttonType,buttonText}) => {
+  return (
+    <div className="w-full max-w-[400px] rounded-lg border-2 border-[#D9E0A4] grid grid-cols-3">
+      <input
+        className="col-span-2 h-full bg-transparent pl-4 outline-none border-none"
+        type={type}
+        placeholder={placeholder}
+      />
+      <button
+        className="col-span-1 border-2 bg-[#D9E0A4] border-[#D9E0A4] hover:bg-[#19485F] hover:border-2 hover:border-[#D9E0A4] hover:text-white text-black my-1 py-1 px-5 rounded-md mr-4 text-sm"
+        type={buttonType}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default InputButton;
--- frontend/src/pages/Consultants.jsx
+++ frontend/src/pages/Consultants.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import PageHead from '../components/PageHead'
 import HeadImage from "../assets/images/head.png"
 import SecondaryButton from '../components/SecondaryButton'
+import InputButton from '../components/InputButton'
 
 const Consultants = () => {
   return (
     <>
     <PageHead title="Find The Consultant you need" image={HeadImage}/>
-    <section>
-      <div className='flex gap-5 items-center'>
+    <section className='flex items-center justify-around my-10'>
+    <div className='flex gap-5 items-center'>
         <h3>Filter By</h3>
-          <select name="industry" id="industry" className="border-2 border-[#D9E0A4] px-6 py-1 capitalize transition hover:border-[#19485F] rounded-md cursor-pointer">
+          <select name="industry" id="industry" className="border-2 text-sm border-[#D9E0A4] px-6 py-1 capitalize transition hover:border-[#19485F] rounded-md cursor-pointer">
             <option disabled>industry</option>
             <option value="tech">tech</option>
             <option value="finance">finance</option>
             <option value="sells">sells</option>
             <option value="marketing">marketing</option>
           </select>
-          <select name="experience" id="experience" className="border-2 border-[#D9E0A4] px-6 py-1 capitalize transition hover:border-[#19485F] rounded-md cursor-pointer">
+          <select name="experience" id="experience" className="border-2 text-sm border-[#D9E0A4] px-6 py-1 capitalize transition hover:border-[#19485F] rounded-md cursor-pointer">
             <option disabled>experience</option>
             <option value="2">2 years</option>
             <option value="5">5 years</option>
@@ -25,6 +26,8 @@ const Consultants = () => {
             <option value="20">20 years</option>
           </select>
       </div>
+
+      <InputButton type="search" placeholder="...Search" buttonType="button" buttonText="Search"/>
     </section>
     </>
   )
