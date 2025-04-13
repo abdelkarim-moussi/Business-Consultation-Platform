f93dbaa37eb4543aa9a5576334ebfca0494c3ecb
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-04-13 13:44:36 +0000

    primary button component

--- frontend/src/components/ExpertsSection.jsx
+++ frontend/src/components/ExpertsSection.jsx
@@ -1,19 +1,21 @@
-import { ExpertCard } from "./ExpertCard";
+import PrimaryButton from "./PrimaryButton"
 import ProfileCard from "./ProfileCard";
 
 export const ExpertsSection = () => {
   return (
-    <div className="py-5">
+    <div className="py-5 text-center">
       <h1 className="text-center text-[6rem] md:text-[8em] leading-[7rem] font-bold mb-20">
         Find the
         <br /> right <span className="text-[#19485F]">expert</span> for you
       </h1>
 
-      <div className="my-5 grid sm:grid-cols-2 md:grid-cols-3 gap-5 sm:px-5 md:px-10">
+      <div className="my-10 grid sm:grid-cols-2 md:grid-cols-3 gap-5 sm:px-5 md:px-10">
         <ProfileCard />
         <ProfileCard />
         <ProfileCard />
       </div>
+
+      <PrimaryButton link="/consultants" text="View More"/>
     </div>
   );
 };
--- frontend/src/components/PrimaryButton.jsx
+++ frontend/src/components/PrimaryButton.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function PrimaryButton(){
+export default function PrimaryButton({link,text,extraClasses}) {
   return (
-    
-  )
+    <Link
+      to={link}
+      className={`bg-[#19485F] border-2 border-[#19485F] px-8 py-1 text-white capitalize transition hover:bg-[#D9E0A4] hover:border-[#D9E0A4] hover:text-[#19485F] rounded-md ${extraClasses}`}
+    >
+      {text}
+    </Link>
+  );
 }
-
